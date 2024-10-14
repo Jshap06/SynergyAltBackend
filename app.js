c6a8ab7413be3face6aecfcccd61d91342445fd9
Jonathan Shapiro <50190184+Jshap06@users.noreply.github.com>
2024-10-14 12:25:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ setInterval(()=>{
 setInterval(async ()=>{
   for(const [domain,states] of viewStates.entries()){
     console.log(domain);console.log(states);
-    await axios.get(domain).then(response=>{
+    await axios.get(domain+"/PXP2_Login_Student.aspx?regenerateSessionId=True").then(response=>{
         const [VIEWSTATE, EVENTVALIDATION]=parseFormData(response.data);
     viewStates.set(domain,[VIEWSTATE,EVENTVALIDATION])}).catch(error=>{console.log(error);})}
     
